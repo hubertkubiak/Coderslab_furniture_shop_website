b82e9f34aeeb8597932a7f843887388e2fe2f033
hubertkubiak <hubkubiak@gmail.com>
2015-11-05 18:33:01 +0100

    adding the dropdown function

--- js/app.js
+++ js/app.js
@@ -1,3 +1,41 @@
 /**
  * Created by Hubert on 2015-10-16.
  */
+
+$(function(){
+
+    var Application = function(){
+        function dropdownMenu(){
+        $(".second-level").css({'opacity' : 0});
+
+            $(".set1").on("mouseenter", function(event){
+                $(".drop1").css({'opacity':1});
+            });
+            $(".first-level").on("mouseleave", function(event){
+                $(".drop1").css({'opacity':0});
+            });
+
+            $(".set2").on("mouseenter", function(event){
+                $(".drop2").css({'opacity':1});
+            });
+            $(".first-level").on("mouseleave", function(event){
+                $(".drop2").css({'opacity':0});
+            });
+
+            $(".set3").on("mouseenter", function(event){
+                $(".drop3").css({'opacity':1});
+            });
+            $(".first-level").on("mouseleave", function(event){
+                $(".drop3").css({'opacity':0});
+            });
+        }
+        return {
+            dropdownMenu: dropdownMenu
+        };
+    };
+
+    var app = new Application();
+    app.dropdownMenu();
+});
+
+
